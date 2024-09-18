c1d1d80ddf5d73cee1803db6fd4be16cf945493d
Albrecht Lohofener <albrechtloh@gmx.de>
2024-09-18 23:51:42 +0200

    Add wan and luci test

--- test/tests/openwrt_docker_test.py
+++ test/tests/openwrt_docker_test.py
@@ -4,6 +4,9 @@ import os
 import subprocess
 import polling2
 import json
+import requests
+import base64
+import time
 
 def is_container_running():
     # Get current service list from supervisor
@@ -52,9 +55,70 @@ def is_service_started(service):
             return False
     else:
         return False
+
+def run_openwrt_shell_command(command, *arg):
+    # Add double quotes
+    command = "\"" + command + "\""
+
+    # Build argument array
+    arguments = "["
+    for val in arg:
+        arguments += "\"" + val + "\","
+    arguments = arguments[:-1] # Remove the last ","
+    arguments += "]"
+
+    # Call qemu guest tools function 'guest-exec'. This is only working if OpenWrt is booted and the qemu guest tools are running
+    process = subprocess.run(['docker','exec','openwrt','sh','-c',r"""echo -ne '{"execute":"guest-exec", "arguments": { "path": """ + command + ", \"arg\":" + arguments + r""","capture-output": true}}' | nc -w 1 -U /run/qga.sock"""], 
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, 
+                         universal_newlines=True)
     
+    if process.returncode != 0:
+        return None
+    
+    if process.stdout == "":
+        return None
+    
+    # Assume json here, parse it
+    ret = json.loads(process.stdout)
+
+    if 'return' not in ret:
+        return None
+    
+    if 'pid' not in ret['return']:
+        return None
+
+    pid = ret['return']['pid']
+
+    # Get stdout of process
+    time.sleep(5) # Give command 5 seconds time to respond. It would be better to implement a real timeout here but I need to got he bed :-(
+    # Call qemu guest tools function 'guest-exec-status'. This is only working if OpenWrt is booted and the qemu guest tools are running
+    process = subprocess.run(['docker','exec','openwrt','sh','-c',r"""echo -ne '{"execute":"guest-exec-status", "arguments": { "pid": """ + str(pid) + r"""}}' | nc -w 1 -U /run/qga.sock"""], 
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, 
+                         universal_newlines=True)
+    
+    if process.returncode != 0:
+        return None
+    
+    if process.stdout == "":
+        return None
+
+    # Assume json here, parse it
+    ret = json.loads(process.stdout)
+    data = ret['return']
+
+    # decode output
+    if 'err-data' in data:
+        data['err-data'] = base64.b64decode(data['err-data']).decode()
+
+    if 'out-data' in data:
+        data['out-data'] = base64.b64decode(data['out-data']).decode()
+
+    return data
+
 def get_openwrt_info():
-    # Get current service list from supervisor
+    # Call qemu guest tools function 'guest-get-osinfo'. This is only working if OpenWrt is booted and the qemu guest tools are running
     process = subprocess.run(['docker','exec','openwrt','sh','-c',r"""echo -ne '{"execute":"guest-get-osinfo"}' | nc -w 1 -U /run/qga.sock"""], 
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE, 
@@ -77,7 +141,7 @@ def get_openwrt_info():
 
     return info['return']
 
-def is_openwrt_loaded():
+def is_openwrt_booted():
     service_status = get_openwrt_info()
 
     if service_status != None:
@@ -86,13 +150,14 @@ def is_openwrt_loaded():
         return False
 
 
-# Tests
+# ************************ Tests ************************
 
 def test_basic_container_start(docker_ip, docker_services):
     docker_services.wait_until_responsive(
         timeout=30.0, pause=0.5, check=lambda: is_container_running()
     )
     return
+
 
 def test_nginx_start(docker_ip, docker_services):
     docker_services.wait_until_responsive(
@@ -101,12 +166,14 @@ def test_nginx_start(docker_ip, docker_services):
     
     assert get_service_status('nginx') == 'RUNNING'
 
+
 def test_openwrt_start(docker_ip, docker_services):
     docker_services.wait_until_responsive(
         timeout=30.0, pause=0.5, check=lambda: is_service_started('openwrt')
     )
     
     assert get_service_status('openwrt') == 'RUNNING'
+
 
 def test_caddy_start(docker_ip, docker_services):
     docker_services.wait_until_responsive(
@@ -115,6 +182,7 @@ def test_caddy_start(docker_ip, docker_services):
 
     assert get_service_status('caddy') == 'RUNNING'
 
+
 def test_script_server_start(docker_ip, docker_services):
     docker_services.wait_until_responsive(
         timeout=30.0, pause=0.5, check=lambda: is_service_started('script-server')
@@ -122,9 +190,10 @@ def test_script_server_start(docker_ip, docker_services):
 
     assert get_service_status('script-server') == 'RUNNING'
 
+
 def test_openwrt_booted(docker_ip, docker_services):
     docker_services.wait_until_responsive(
-        timeout=90.0, pause=1, check=lambda: is_openwrt_loaded()
+        timeout=90.0, pause=1, check=lambda: is_openwrt_booted()
     )
 
     info = get_openwrt_info()
@@ -132,13 +201,37 @@ def test_openwrt_booted(docker_ip, docker_services):
 
     assert 'OpenWrt' == info['name']
 
+
 def test_openwrt_lan_veth_pair(docker_ip, docker_services):
     docker_services.wait_until_responsive(
-        timeout=30.0, pause=0.5, check=lambda: is_service_started('openwrt')
+        timeout=90.0, pause=1, check=lambda: is_openwrt_booted()
     )
     
-    assert get_service_status('openwrt') == 'RUNNING'
-
     response = polling2.poll(lambda: os.system("ping -c 1 172.31.1.1") == 0, step=1, timeout=90)
     
     assert response == True
+
+
+def test_openwrt_wan_host(docker_ip, docker_services):
+    docker_services.wait_until_responsive(
+        timeout=90.0, pause=1, check=lambda: is_openwrt_booted()
+    )
+    
+    response = run_openwrt_shell_command("ping", "-w2", "-W2", "-c1", "google.com")
+    print(response)
+    
+    # Exitcode 0 means every is going fine. Ping was successfull
+    assert response['exitcode'] == 0
+
+
+def test_openwrt_luci_forwarding(docker_ip, docker_services):
+    docker_services.wait_until_responsive(
+        timeout=90.0, pause=1, check=lambda: is_openwrt_booted()
+    )
+    
+    # Double check if caddy is still running
+    assert get_service_status('caddy') == 'RUNNING'
+
+    response = requests.get("http://localhost:9000")
+    
+    assert ('LuCI - Lua Configuration Interface' in response.content.decode()) == True
