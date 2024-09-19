04701f2757f1723cc31b4dbe30d41885a1716d2c
Albrecht Lohofener <albrechtloh@gmx.de>
2024-09-19 08:16:59 +0200

    update

--- test/tests/openwrt_docker_test.py
+++ test/tests/openwrt_docker_test.py
@@ -207,9 +207,12 @@ def test_openwrt_lan_veth_pair(docker_ip, docker_services):
         timeout=90.0, pause=1, check=lambda: is_openwrt_booted()
     )
     
-    response = polling2.poll(lambda: os.system("ping -c 1 172.31.1.1") == 0, step=1, timeout=90)
+    try:
+        response = polling2.poll(lambda: os.system("ping -c 1 172.31.1.1") == 0, step=1, timeout=90)
+    except polling2.TimeoutException:
+        assert False
     
-    assert response == True
+    pass
 
 
 def test_openwrt_wan_host(docker_ip, docker_services):
@@ -217,17 +220,11 @@ def test_openwrt_wan_host(docker_ip, docker_services):
         timeout=90.0, pause=1, check=lambda: is_openwrt_booted()
     )
     
-    try:
-        # Exitcode 0 means every is going fine. Ping was successfull
-        response = polling2.poll(lambda: run_openwrt_shell_command("ping", "-w2", "-W2", "-c1", "google.com")['exitcode'] == 0, step=10, timeout=90)
-    except polling2.TimeoutException:
-        # Get ping error message to run it again
-        response = run_openwrt_shell_command("ping", "-w2", "-W2", "-c1", "google.com")
-        print(response)
-
-        assert False
+    # For some reason ping is not working at github actions, so use nslookup to test internet connection
+    response = run_openwrt_shell_command("nslookup", "google.com")
+    print(response)
     
-    pass
+    assert response['exitcode'] == 0
 
 
 def test_openwrt_luci_forwarding(docker_ip, docker_services):
